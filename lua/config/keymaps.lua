c40ff6f7d7176b53b082a0c7ae7fe56eec197046
Jakir Hossain <js.hossain256@gmail.com>
2026-04-23 18:26:18 -0400

    nvim init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>wq", ":wq<CR>")
