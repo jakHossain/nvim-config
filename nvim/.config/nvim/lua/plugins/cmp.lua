4c4c621eb347521caad6682080f3a437c8d5f147
Jakir Hossain <js.hossain256@gmail.com>
2026-05-02 22:03:05 -0400

    update cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -5,12 +5,17 @@ return {
     "hrsh7th/cmp-nvim-lsp",     -- Connects cmp to your language servers
     "L3MON4D3/LuaSnip",         -- Snippet engine (required by cmp)
     "saadparwaiz1/cmp_luasnip", -- Connects luasnip to cmp
+    "hrsh7th/cmp-nvim-lsp-signature-help",
   },
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
 
     cmp.setup({
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
@@ -25,6 +30,7 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" }, -- Prioritize LSP suggestions (types, methods)
         { name = "luasnip" },  -- Then snippets
+        { name = 'nvim_lsp_signature_help' }, -- Add this line
       })
     })
   end,
