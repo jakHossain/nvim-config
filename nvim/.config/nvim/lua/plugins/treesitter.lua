b3501458a4de91c78e67a892fcc51643fdde9ea4
Jakir Hossain <js.hossain256@gmail.com>
2026-07-31 00:16:09 -0400

    treesitter better tag editing

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,47 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      -- Native Neovim hook to turn on the engine for recognized file types
-      vim.api.nvim_create_autocmd("FileType", {
-        group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true }),
-        callback = function()
-          pcall(vim.treesitter.start)
-        end,
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+			"andymass/vim-matchup",
+		},
+		config = function()
+			vim.filetype.add({ extension = { svg = "xml" } })
+			vim.treesitter.language.register("xml", "svg")
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true }),
+				callback = function()
+					pcall(vim.treesitter.start)
+				end,
+			})
+
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = true,
+				},
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		event = "BufReadPost",
+		config = function()
+			local textobjects = {
+				["at"] = "@element.outer",
+				["it"] = "@element.inner",
+			}
+
+			for key, capture in pairs(textobjects) do
+				vim.keymap.set({ "o", "x" }, key, function()
+					require("nvim-treesitter-textobjects.select").select_textobject(capture, "textobjects")
+				end, { desc = "Select " .. capture })
+			end
+		end,
+	},
 }
